4fcfa65dc5159e33cfa76aa91ee06638d49ca5d2
kaualor <guardakaua@gmail.com>
2024-09-09 23:17:12 -0300

    Projeto R6

---
+++ dados.js
@@ -0,0 +1,24 @@
+let dados = [
+    {
+        titulo: "Kapkan", 
+        descricao: "Kapkan é um agente que utiliza armadilhas e é uma adição mortal à equipe defensora. Kapkan é equipado com um Dispositivo bloqueador de entrada (EDD-MK II). Esta armadilha é uma carga de C4 compactada que aciona ao detectar movimento. Pode ser colocada em molduras de portas e janelas, negando acesso a entradas estratégicas dos atacantes.",
+        link: "https://www.ubisoft.com/pt-br/game/rainbow-six/siege/game-info/operators/kapkan"
+    },
+    {
+        titulo: "Ash",
+        descricao: "Ash é uma agente atacante rápida e precisa, equipada com o Lançador de Granada Breaching Round M120 CREM. Ela utiliza suas granadas explosivas para destruir barricadas e paredes não reforçadas, permitindo rápidas entradas e flanqueios. Sua agilidade e habilidade em combate de longa e curta distância fazem dela uma escolha versátil para ofensivas dinâmicas.",
+        link: "https://www.ubisoft.com/pt-br/game/rainbow-six/siege/game-info/operators/ash"
+    },
+    {
+        titulo: "Sledge",
+        descricao: "Sledge é um operador atacante que utiliza seu Martelo de Demolição Tático (The Caber) para destruir barricadas, paredes não reforçadas e outras superfícies destrutíveis. Sua habilidade permite criar novas linhas de visão e abrir caminhos para a equipe, tornando-o essencial para estratégias de penetração e surpresa.",
+        link: "https://www.ubisoft.com/pt-br/game/rainbow-six/siege/game-info/operators/sledge"
+    },
+    {
+        titulo: "Jäger",
+        descricao: "Jäger é um operador defensor especializado em neutralizar ameaças aéreas. Ele é equipado com o Sistema de Defesa Ativa ADS-MKIV 'Magpie', que pode interceptar e destruir granadas e outros dispositivos lançados pelos atacantes. Sua habilidade de eliminar projéteis inimigos aumenta significativamente a segurança das áreas defendidas.",
+        link: "https://www.ubisoft.com/pt-br/game/rainbow-six/siege/game-info/operators/jager"
+    }
+];
+
+
